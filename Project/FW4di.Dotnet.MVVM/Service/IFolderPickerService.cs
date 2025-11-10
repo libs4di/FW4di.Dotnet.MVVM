9ae288ffa63dcc6684a40c3efb8ba7f2906d226f
Robert Oze <support@4dillusions.com>
2025-11-10 17:55:48 +0100

    Create IFolderPickerService.cs

---
+++ Project/FW4di.Dotnet.MVVM/Service/IFolderPickerService.cs
@@ -0,0 +1,12 @@
+﻿/*
+4di Framework .NET MVVM Library
+Copyright (c) 2025 by 4D Illusions. All rights reserved.
+Released under the terms of the GNU General Public License version 3 or later.
+*/
+
+namespace FW4di.Dotnet.MVVM.Service;
+
+public interface IFolderPickerService
+{
+    Task<string?> PickFolderAsync();
+}
